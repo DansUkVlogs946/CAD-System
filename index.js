e4c6417dc94d585f4de1c2fe542cff888606403c
DansUkVlogs946 <82105053+DansUkVlogs946@users.noreply.github.com>
2025-03-13 16:41:10 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ function sendButtonData(buttonName) {
     })
     .then(response => {
       console.log("Request sent successfully!");
-      alert("Data sent successfully!");
     })
     .catch(error => {
       console.error("Error:", error);
@@ -36,5 +35,46 @@ function sendButtonData(buttonName) {
     button.addEventListener("click", function() {
       var buttonName = this.getAttribute("data-button");
       sendButtonData(buttonName);
+      changeButtonColor(buttonName);
+    });
+  });
+
+  function changeButtonColor(buttonName) {
+    var elements = document.querySelectorAll("button");   
+
+    for(var i = 0, len = elements.length; i < len; i++) {   
+        elements[i].style.color = "white";
+    }
+
+
+    document.getElementById(buttonName.replace(/\s+/g, '')).style.color = "lightgreen";
+  }
+
+  // Button to remove the user
+document.getElementById("removeButton").addEventListener("click", function() {
+    var user = document.getElementById("userCallSign").value;
+    if (!user) {
+      alert("Please enter a callsign.");
+      return;
+    }
+  
+    fetch("https://script.google.com/macros/s/AKfycby6kz8a8f347JSvWdb1hRFDtKwEHUuIy_QGGsgAnGfBiDuZ5kGf8xprr89LPal0XdV9/exec", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        user: user,  // Pass the callsign to be removed
+        button: "remove"  // We can use this to differentiate between adding and removing data
+      })
+    })
+    .then(response => response.text())
+    .then(data => {
+      console.log("Success:", data);
+      alert(data);  // Show success or failure message
+    })
+    .catch(error => {
+      console.error("Error:", error);
+      alert("Error occurred!");
     });
   });
